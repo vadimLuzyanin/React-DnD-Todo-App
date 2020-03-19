e1b1e21dc1c69407249c0d3c869dec34bce692bb
vadimLuzyanin <luzianin999@inbox.ru>
2020-03-19 16:41:02 +0300

    Updates

---
+++ precache-manifest.51f59b249a63ba98f4e475ac29c867b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fb692cef68cb467ffedb60f8a95352",
+    "url": "/React-DnD-Todo-App/index.html"
+  },
+  {
+    "revision": "b01d334e5f07e54b0e64",
+    "url": "/React-DnD-Todo-App/static/js/2.5e722166.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/React-DnD-Todo-App/static/js/2.5e722166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b4797525ef4e2c561d",
+    "url": "/React-DnD-Todo-App/static/js/main.8c83021d.chunk.js"
+  },
+  {
+    "revision": "687dc92412ed5d2177b7",
+    "url": "/React-DnD-Todo-App/static/js/runtime-main.f206a944.js"
+  }
+]);
